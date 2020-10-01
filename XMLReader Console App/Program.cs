da60754020178f920633d32300511bdf536eeef9
karol.grela <karol.grela@vp.pl>
2020-10-01 15:55:41 +0200

    Add project files.

---
+++ XMLReader Console App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XMLReader_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string version = "3", area = "XYZ", status = "dobry";
+            FileInfo info = new FileInfo();
+            info.Version = version;
+            info.Production_area = area;
+            info.Status = status;
+
+            Console.WriteLine($"{info.Version}\n{info.Production_area}\n{info.Status}");
+        }
+    }
+}
